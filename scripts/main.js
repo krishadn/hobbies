b52b254fdb699e62d9615f0165be66d40df3e9a9
krishadn <krizziasantillan@gmail.com>
2025-09-16 19:30:37 +0800

    added main page navigation functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,97 @@
-// functions
+/* 
+    to fetch content from json file
+*/
+const filename = "content.json";
+const entries = []
+let offset = 0;
+const entryPerPage = 5;
 
-// ------------- to create article components -------------
+fetch(filename)
+    .then((contents) => contents.json())
+    .then((contentsJson) => entries.push(...contentsJson.entries))
+    .then(loadContent);
+    
+
+/* functions for loading content */
+
+function loadContent() {
+    const main = document.querySelector(".main");
+    clearMain();
+
+    let index = 0;
+    while (index != entryPerPage && entries[offset+index]) {
+        main.append(createArticle(entries[offset+index]))
+        index++
+    }
+
+    const navDiv = document.createElement("div");
+    navDiv.classList.add("main__page-nav");
+
+    if (entries[offset+index]) {
+        navDiv.append(createByPageNav("next"));
+    }
+
+    if (offset !== 0) {
+        navDiv.append(createByPageNav("previous"));
+        navDiv.append(createEndNav("first"));
+    }
+
+    if (entries[offset+index]) {
+        navDiv.append(createEndNav("last"));
+    }
+
+    navDiv.addEventListener("click", handlePageNav)
+    main.append(navDiv);
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    })
+}
+
+
+function handlePageNav(e) {
+    
+    if (e.target.tagName === "SPAN"){
+        if (e.target.textContent.includes("next")) {
+            offset += entryPerPage;
+            loadContent();
+        } else if (e.target.textContent.includes("previous")) {
+            offset -= entryPerPage;
+            loadContent();
+        } else if (e.target.textContent.includes("first")) {
+            offset = 0;
+            loadContent();
+        } else if (e.target.textContent.includes("last")) {
+            const fullPages = Math.floor(entries.length / entryPerPage)
+            offset = fullPages * entryPerPage;
+            loadContent();
+        } 
+    }
+
+}
+
+
+/* utility functions */
+
+function clearMain() {
+    const main = document.querySelector(".main");
+
+    if (main.children[0]) {
+        while (main.children[0]) {
+            main.children[0].remove();
+        }
+    }
+
+}
+
+
+
+
+/* functions for components */
+
+/* ------------- Article component ------------- */
+
 function createArticleTitle(title) {
     const titleDiv = document.createElement("div");
     const titleText = document.createElement("h2");
@@ -64,12 +155,11 @@ function createPhotoContent(entry) {
 }
 
 function createArticle(entry) {
-    const main = document.querySelector("main");
     const article = document.createElement("article");
     article.classList.add("main__article");
 
     const title = createArticleTitle(entry.title);
-    let content;
+    let content = document.createTextNode("No content");;
     
     switch (entry.category) {
         case "writings":
@@ -85,16 +175,53 @@ function createArticle(entry) {
 
     article.appendChild(title);
     article.appendChild(content);
-    main.appendChild(article);
+    return article;
 }
 
-// ------------- to fetch content -------------
-async function getContent(filename) {
-    const contents = await fetch(filename);
-    const contentsJson = await contents.json();
-    contentsJson.entries.forEach(createArticle);
+
+/* ------------- Page navigatoffset = ion component ------------- */
+
+// for Next and Previous
+function createByPageNav(text) {
+    const par = document.createElement("p");
+    par.classList.add(text);
+
+    const span = document.createElement("span");
+    span.append(text);
+    span.append(document.createElement("br"));
+    span.append("page");
+ 
+    par.append(span)
+
+    return par;
 }
 
-// main script
-const filename = "content.json";
-getContent(filename)
+// for First and Last
+function createEndNav(text) {
+    const par = document.createElement("p");
+    par.classList.add(text);
+
+    const span = document.createElement("span");
+    span.append(`${text} page`);
+    par.append(span)
+
+    return par;
+}
+
+function createPageNav() {
+    const navDiv = document.createElement("div");
+    navDiv.classList.add("main__page-nav");
+    navDiv.append(createByPageNav("next"));
+    navDiv.append(createByPageNav("previous"));
+    navDiv.append(createEndNav("first"));
+    navDiv.append(createEndNav("last"));
+    return navDiv;
+}
+
+
+
+
+
+
+
+
